7a40f8becf4dc7f51dc634c31bc19978cbea65d4
dichika <ichikawadaisuke@gmail.com>
2014-07-11 20:38:18 +0900

    First

--- src/Yo.jl
+++ src/Yo.jl
@@ -1,5 +1,14 @@
 module Yo
 
-# package code goes here
+using HTTPClient.HTTPC
+using JSON
+
+function yo(apitoken)
+  res=HTTPC.post("http://api.justyo.co/yoall/",[("api_token",apitoken)])
+end
+
+function yo(apitoken,username)
+  res=HTTPC.post("http://api.justyo.co/yo/",[("api_token",apitoken),("username",username)])
+end
 
 end # module
